49451dbf19dc9205f21e624dd0f5ab52cbf07650
Alibriaan <dimulka62@gmail.com>
2019-08-04 22:27:11 +0300

    Updates

---
+++ precache-manifest.103c9e778f30e0062097d3a401c7633d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8840ec77fe4a2bcf0622a50af38f14",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "c96efe05a6d05d54afb2",
+    "url": "/Pokedex/static/css/main.73090df2.chunk.css"
+  },
+  {
+    "revision": "cef085bf315c17ecf6ae",
+    "url": "/Pokedex/static/js/2.045e52d4.chunk.js"
+  },
+  {
+    "revision": "c96efe05a6d05d54afb2",
+    "url": "/Pokedex/static/js/main.c731d924.chunk.js"
+  },
+  {
+    "revision": "119fa3083960fadc0325",
+    "url": "/Pokedex/static/js/runtime~main.cf3900f6.js"
+  }
+]);
